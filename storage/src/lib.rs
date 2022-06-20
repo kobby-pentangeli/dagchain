829582744668d1c317008f7a6209f373d3985af7
Kobby Pentangeli <kobbypentangeli@gmail.com>
2022-06-20 06:42:52 +0000

    `storage`: add Storage trait and StorageType enum

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,3 +1,29 @@
 #![warn(clippy::all)]
 
+use crypto::hash::Hash;
+use serde::{Deserialize, Serialize};
+
 pub mod error;
+pub use error::StorageError;
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum StorageType {
+    Memory,
+    Sled,
+}
+
+pub trait Storage: Send + Sync {
+    /// Create new storage
+    fn new(path: Option<&std::path::Path>) -> Result<Self, StorageError>
+    where
+        Self: std::marker::Sized;
+
+    /// Insert data
+    fn insert(&mut self, key: Hash, value: Vec<u8>) -> Result<(), StorageError>;
+
+    /// Get data
+    fn get(&self, key: Hash) -> Result<Vec<u8>, StorageError>;
+
+    /// Flush data
+    fn flush(&mut self) -> Result<(), StorageError>;
+}
